64fd9170f3a66f65fd78ea154b67eeed7e0e3823
Coral Winter <wj_0575@sjtu.edu.cn>
2025-03-27 00:00:23 +0800

    Update main.py

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ def create_note(text):
         # 如果 Label 组件不存在，创建新的 Label 组件
         # 定位在右下角
         info_label = tk.Label(root, text=text, bg=color_dark_blue, fg=color_white, font=("黑体", 10))
-        info_label.place(relx=0.5, rely=0.97, relwidth=1, relheight=0.04, anchor="center")
+        info_label.place(relx=0.5, rely=0.98, relwidth=1, relheight=0.04, anchor="center")
 
 
 # 显示答案的地方
@@ -71,36 +71,37 @@ equation_text.place(relx=0.5, rely=0.25, relwidth=0.95, relheight=0.4, anchor="c
 variable_frame = tk.Entry(equation_frame, bg=color_white, fg=color_black, font=("Arial", 20))
 variable_frame.place(relx=0.3, rely=0.75, relwidth=0.55, relheight=0.4, anchor="center")
 # 提示文本
-equation_inform = tk.Label(equation_frame, text="←变量   表达式↑ 计算模式→",
+equation_inform = tk.Label(equation_frame, text="←变量  表达式↑  计算模式→",
                            bg=color_grey, fg=color_black, font=("黑体", 16))
 equation_inform.place(relx=0.73, rely=0.75, relwidth=0.29, relheight=0.4, anchor="center")
 # 在equation_frame中添加一个按钮，用于切换计算模式
 realness_or_fraction = False  # 初始状态为fraction有理数模式
 
 tmp_text = {}
+tmp_frame = tk.Frame(root, bg=color_grey)
+tmp_frame.place(relx=0.8, rely=0.6, relwidth=0.35, relheight=0.45, anchor="center")
 for i in range(10):
     if i%2 == 0:
-        color = color_dark_grey
+        color = color_light_brown
     else:
-        color = color_grey
-    tmp_text[i] = tk.Entry(root, bg=color, fg=color_black, font=("Arial", 11))
-    tmp_text[i].place(relx=0.8, rely=0.4+0.04*i, relwidth=0.35, relheight=0.04, anchor="center")
+        color = color_light_earth
+    tmp_text[i] = tk.Entry(tmp_frame, bg=color, fg=color_black, font=("Arial", 12))
+    tmp_text[i].place(relx=0.5, rely=0.1+0.09*i, relwidth=0.95, relheight=0.09, anchor="center")
 
 
 def calculate_mode_button():
     global realness_or_fraction
     if realness_or_fraction:
         create_note("有理数模式 结果精确 不能开分数次方")
-        search_link_fuzzy_button.config(bg=color_light_pink, text="有理数")
+        calculate_mode_button.config(bg=color_light_pink, text="有理数")
         realness_or_fraction = False
     else:
-        create_note("浮点数模式 内嵌常见常数 有位数限制 注意浮点误差")
-        search_link_fuzzy_button.config(bg=color_light_purple, text="浮点数")
+        create_note("浮点数模式 内嵌常见常数 注意浮点误差")
+        calculate_mode_button.config(bg=color_light_blue, text="浮点数")
         realness_or_fraction = True
-
-search_link_fuzzy_button = tk.Button(equation_frame, text="有理数", bg=color_light_pink,
-                                     command=calculate_mode_button, font=("黑体", 16))
-search_link_fuzzy_button.place(relx=0.93, rely=0.75, relwidth=0.09, relheight=0.4, anchor="center")
+calculate_mode_button = tk.Button(equation_frame, text="有理数", bg=color_light_pink,
+                                  command=calculate_mode_button, font=("黑体", 16))
+calculate_mode_button.place(relx=0.93, rely=0.75, relwidth=0.09, relheight=0.4, anchor="center")
 
 
 create_head("结果在这里")
